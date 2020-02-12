ddade7df1bae317857c05618911f9568f83821dc
GoldSwan <saltmenu@naver.com>
2020-02-12 14:18:12 +0900

    Update 01_StringExample, 02_listExample

---
+++ 01_stringExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace stringExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Replace : 문자열 치환
+            string sayIntro = "I am swan";
+            Console.WriteLine(sayIntro);
+            sayIntro = sayIntro.Replace("swan", "blackswan");
+            Console.WriteLine(sayIntro);//I am blackswan
+
+            //문자열 검색
+            string songLyrics = "happy birthday to you";
+
+            //Contains : 문자열포함여부
+            Console.WriteLine(songLyrics.Contains("birthday"));
+            Console.WriteLine(songLyrics.Contains("monday"));
+            //StartsWith : 문자열 시작을 기준으로 문자열포함여부
+            Console.WriteLine(songLyrics.StartsWith("happy"));
+            Console.WriteLine(songLyrics.StartsWith("sad"));
+            //EndsWith : 문자열 끝을 기준으로 문자열포함여부
+            Console.WriteLine(songLyrics.EndsWith("you"));
+            Console.WriteLine(songLyrics.EndsWith("me"));
+
+            //문자열 대소문자 변환
+            Console.WriteLine(songLyrics.ToUpper());
+            Console.WriteLine(songLyrics.ToLower());
+        }
+    }
+}
---
+++ 02_listExample/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace listExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //목록(List Collections)
+            var names = new List<string> { "Bob", "Alice", "Happy" };
+
+            //List Add, Remove
+            names.Add("Sam");
+            names.Add("Joker");
+            names.Remove("Happy");
+
+            foreach (var name in names)
+            {
+                Console.WriteLine("Hello! " + name);
+            }
+
+            //Index로 바로 접근 가능
+            Console.WriteLine(names[2]);
+
+            //List IndexOf로 값에 해당하는 index return
+            var index = names.IndexOf("Alice");
+            if (index != -1)
+                Console.WriteLine(names[index] + ":" + index);
+
+            index = names.IndexOf("Trudy");
+            Console.WriteLine(index);//찾을 수 없는 경우 -1 return
+
+            //List 정렬
+            names.Sort();
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name.ToUpper());
+            }
+
+            Program.makeFibonacciNumbers(10);//피보나치 수열 생성
+        }
+
+        static void makeFibonacciNumbers(int size)
+        {
+            //List 를 통한 피보나치 수열 생성
+            var fibonacciNumbers = new List<int> { 1, 1 };
+
+            while (fibonacciNumbers.Count < size)
+            {
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                fibonacciNumbers.Add(previous + previous2);
+            }
+
+            foreach (var item in fibonacciNumbers)
+                Console.WriteLine(item);
+        }
+    }
+}
