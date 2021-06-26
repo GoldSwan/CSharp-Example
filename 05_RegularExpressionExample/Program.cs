442d53da0527a25d276ad1ca236fdb9c9eebaa3d
goldswan <saltmenu@naver.com>
2021-06-26 12:37:50 +0900

    [FIX]fix code comments

--- 05_RegularExpressionExample/Program.cs
+++ 05_RegularExpressionExample/Program.cs
@@ -15,8 +15,9 @@ namespace RegularExpressionExample
         static void RegPartternExample1()
         {
             Console.WriteLine("예제1");
+            //앞뒤 쌍따옴표, 마지막 개행 제거하는 정규표현식
             string pattern = "^\"|\"$|\"\r\n$";
-            string input = "\"개행된\r\n문자\r\n입니다.\"\r\n";//앞뒤 쌍따옴표, 마지막 개행 제거하는 정규표현식
+            string input = "\"개행된\r\n문자\r\n입니다.\"\r\n";
             Console.WriteLine("변경전");
             Console.WriteLine(input);
             Console.WriteLine();
@@ -35,7 +36,8 @@ namespace RegularExpressionExample
         {
             Console.WriteLine("예제2");
             string str = "나열된 단어1 단어2 단어3 단어4 입니다.";
-            Regex regex = new Regex(@"\b(\w+?)\b", RegexOptions.IgnoreCase);//단어별로 구분하는 정규표현식
+            //단어별로 구분하는 정규표현식
+            Regex regex = new Regex(@"\b(\w+?)\b", RegexOptions.IgnoreCase);
 
             foreach (Match m in regex.Matches(str))
             {
@@ -47,8 +49,10 @@ namespace RegularExpressionExample
         static void RegPartternExample3()
         {
             Console.WriteLine("예제3");
-            string[] strArray = new string[5] { "file file1.txt", "file.file2.jpg", "file3.png", "file4.dll", "file5.jpeg" };
-            Regex regex = new Regex(@"\.(jpeg|gif|png|jpg)$", RegexOptions.IgnoreCase);//파일명 확장자가 이미지인지 체크하는 정규표현식
+            string[] strArray = new string[5] { "file file1.txt", "file.file2.jpg", 
+                                                "file3.png", "file4.dll", "file5.jpeg" };
+            //파일명 확장자가 이미지인지 체크하는 정규표현식
+            Regex regex = new Regex(@"\.(jpeg|gif|png|jpg)$", RegexOptions.IgnoreCase);
 
             foreach (string str in strArray)
             {
